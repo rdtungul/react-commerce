85dee619028219379253eb40136333f1fd2035ef
rdtungul <rosephdarl@gmail.com>
2024-01-22 23:36:35 +0800

    styled components basics

--- src/App.js
+++ src/App.js
@@ -3,28 +3,13 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { Navbar, Sidebar, Footer } from "./components";
 
 // importing styled components
-import styled from "styled-components";
+import Testing from "./testing";
 
-const Button = styled.button`
-  background: green;
-  color: black;
-`;
-
-const Container = styled.div`
-  background: red;
-  color: white;
-  font-size: 2rem;
-`;
 function App() {
   return (
     <div>
       <h4>React Commerce</h4>
-      <Button>Click me</Button>
-      <Container>
-        <div>
-          <h3>hello world</h3>
-        </div>
-      </Container>
+      <Testing />
     </div>
   );
 }
