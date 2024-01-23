7bad4945528939df7aa275dcc61d9bec1d67efc1
rdtungul <rosephdarl@gmail.com>
2024-01-23 11:47:59 +0800

    setting up react router

--- src/App.js
+++ src/App.js
@@ -2,15 +2,50 @@ import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { Navbar, Sidebar, Footer } from "./components";
 
-// importing styled components
-import Testing from "./testing";
+import {
+  Home,
+  Products,
+  SingleProduct,
+  About,
+  Cart,
+  Error,
+  Checkout,
+  PrivateRoute,
+} from "./pages";
 
 function App() {
   return (
-    <div>
-      <h4>React Commerce</h4>
-      <Testing />
-    </div>
+    <Router>
+      {/* Navbar/Sidebar */}
+      <Navbar />
+      <Sidebar />
+
+      {/* React Router */}
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route exact path="/about">
+          <About />
+        </Route>
+        <Route exact path="/cart">
+          <Cart />
+        </Route>
+        <Route exact path="/products">
+          <Products />
+        </Route>
+        <Route exact path="/products/:id" children={<SingleProduct />} />
+        <Route exact path="/checkout">
+          <Checkout />
+        </Route>
+        <Route exact path="*">
+          <Error />
+        </Route>
+      </Switch>
+
+      {/* Footer */}
+      <Footer />
+    </Router>
   );
 }
 
